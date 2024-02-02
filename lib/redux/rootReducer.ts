8274a9ba2cd422a34f9ffc15b2cb29ee449727b5
govindKulk <kulkarnigovind2003@gmail.com>
2024-02-02 22:42:54 +0530

    practiced store creation

--- lib/redux/rootReducer.ts
+++ lib/redux/rootReducer.ts
@@ -1,6 +1,7 @@
 /* Instruments */
-import { counterSlice } from "./slices";
+import { counterSlice, postSlice } from "./slices";
 
 export const reducer = {
   counter: counterSlice.reducer,
+  post: postSlice.reducer
 };
--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -4,6 +4,7 @@ import {
   type ThunkAction,
   type Action,
 } from "@reduxjs/toolkit";
+
 import {
   useSelector as useReduxSelector,
   useDispatch as useReduxDispatch,
@@ -14,19 +15,23 @@ import {
 import { reducer } from "./rootReducer";
 import { middleware } from "./middleware";
 
+
 export const reduxStore = configureStore({
   reducer,
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(middleware);
+        return getDefaultMiddleware().concat(middleware);
   },
-});
+})
 
-export const useDispatch = () => useReduxDispatch<ReduxDispatch>();
-// export const useDispatch: () => ReduxDispatch = useReduxDispatch;
-export const useSelector: TypedUseSelectorHook<ReduxState> = useReduxSelector;
 
 /* Types */
-export type ReduxStore = typeof reduxStore;
+/*
+  1. ReduxState: type of entire global stae
+  2. ReduxDispatch: type of dispatch function
+  3. ReduxThunkAction: type of thunk action creator function
+  4. useDispatch: typed version of useDispatch according to our store
+  5. useSelector: typed version of useSelector according to our store and state
+*/
 
 export type ReduxState = ReturnType<typeof reduxStore.getState>;
 export type ReduxDispatch = typeof reduxStore.dispatch;
@@ -35,7 +40,12 @@ export type ReduxThunkAction<ReturnType = void> = ThunkAction<
   ReduxState,
   unknown,
   Action
->;
+>
+
+
+export const useDispatch = () => useReduxDispatch<ReduxDispatch>();
+// export const useDispatch: () => ReduxDispatch = useReduxDispatch;
+export const useSelector: TypedUseSelectorHook<ReduxState> = useReduxSelector;
 
 
 
